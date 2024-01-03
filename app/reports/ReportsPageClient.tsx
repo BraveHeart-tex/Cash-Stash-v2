cbf735cbebc26131f843680b34bb8cd2f23b3541
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-03 23:23:34 +0300

    add animations on reports page client

--- app/reports/ReportsPageClient.tsx
+++ app/reports/ReportsPageClient.tsx
@@ -3,6 +3,7 @@ import ReportTable from "../components/ReportsPage/ReportTable";
 import { getGenericListByCurrentUser } from "@/actions/generic";
 import { SerializedUserAccount } from "../redux/features/userAccountSlice";
 import { SearchParams } from "../transactions/page";
+import MotionDiv from "@/components/animation/MotionDiv";
 
 const ReportsPageClient = async ({
   searchParams,
@@ -37,12 +38,19 @@ const ReportsPageClient = async ({
 
   return (
     <div className="p-4 mx-auto lg:max-w-[1300px] xl:max-w-[1600px]">
-      <h3 className="text-4xl mb-4 text-primary">Reports</h3>
-      <ReportTable
-        monthlyTransactionsData={data}
-        currentUserAccounts={userAccountsResult}
-        transactions={userTransactionsResult}
-      />
+      <MotionDiv
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0, x: 200, scale: 1.2 }}
+        transition={{ duration: 0.5, type: "just" }}
+      >
+        <h3 className="text-4xl mb-4 text-primary">Reports</h3>
+        <ReportTable
+          monthlyTransactionsData={data}
+          currentUserAccounts={userAccountsResult}
+          transactions={userTransactionsResult}
+        />
+      </MotionDiv>
     </div>
   );
 };
