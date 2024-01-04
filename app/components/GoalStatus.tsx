2b806c3dfd9612b36a78f202b7128a6a216e3d29
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-04 18:34:53 +0300

    small animation fix on goal status

--- app/components/GoalStatus.tsx
+++ app/components/GoalStatus.tsx
@@ -2,15 +2,20 @@ import { SerializedGoal } from "@/app/redux/features/goalSlice";
 import GoalCard from "./GoalCard";
 import CreateGoalButton from "./CreateButtons/CreateGoalButton";
 import AnimatePresenceClient from "@/components/animation/AnimatePresence";
+import MotionDiv from "@/components/animation/MotionDiv";
 
 const GoalStatus = ({ goals }: { goals: SerializedGoal[] }) => {
   if (goals.length === 0) {
     return (
       <article className="flex h-[300px] items-center justify-center">
-        <div>
+        <MotionDiv
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <p className="text-primary">No goals found.</p>
           <CreateGoalButton />
-        </div>
+        </MotionDiv>
       </article>
     );
   }
