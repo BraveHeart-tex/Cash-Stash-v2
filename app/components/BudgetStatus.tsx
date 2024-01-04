f71ac61a67c6332460b97dc9bbebe572e39e5550
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-04 18:33:12 +0300

    small fix on animation in budgets

--- app/components/BudgetStatus.tsx
+++ app/components/BudgetStatus.tsx
@@ -2,21 +2,26 @@ import { SerializedBudget } from "@/app/redux/features/budgetSlice";
 import BudgetCard from "./BudgetCard";
 import CreateBudgetButton from "./CreateButtons/CreateBudgetButton";
 import AnimatePresenceClient from "@/components/animation/AnimatePresence";
+import MotionDiv from "@/components/animation/MotionDiv";
 
 const BudgetStatus = ({ budgets }: { budgets: SerializedBudget[] }) => {
   if (!budgets || budgets.length === 0) {
     return (
       <article className="flex h-[300px] items-center justify-center">
-        <div>
+        <MotionDiv
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <p className="text-primary">No budgets found.</p>
           <CreateBudgetButton />
-        </div>
+        </MotionDiv>
       </article>
     );
   }
 
   return (
-    <div>
+    <div className="space-y-2">
       <AnimatePresenceClient>
         {budgets.map((budget) => (
           <BudgetCard key={budget.id} budget={budget} />
