779fc14a5383c593be563632e3b06a922cd378b2
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-02-15 21:29:43 +0300

    Add search input component to goals page

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -3,6 +3,7 @@ import CreateGoalButton from "@/components/CreateButtons/CreateGoalButton";
 import GoalCard from "@/components/GoalCard";
 import { getPaginatedGoalsAction } from "@/actions";
 import RoutePaginationControls from "@/components/route-pagination-controls";
+import RouteSearchInput from "@/components/route-search-input";
 
 const GoalsPage = async ({
   searchParams,
@@ -17,25 +18,24 @@ const GoalsPage = async ({
   return (
     <main>
       <div className="p-4 mx-auto lg:max-w-[1300px] xl:max-w-[1600px]">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-4 lg:mb-0">
           <h2 className="text-4xl lg:mb-4 text-primary">Goals</h2>
           <CreateGoalButton className="mt-0" />
         </div>
-        <div className="flex justify-center lg:items-center flex-col gap-2">
+        <RouteSearchInput placeholder="Search goals by name" />
+        <div className="h-[500px] lg:pr-4 mt-2 lg:mt-0 overflow-auto w-full">
           {result.goals.length > 0 ? (
-            <div className="w-full">
-              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
-                {result.goals.map((goal) => (
-                  <GoalCard goal={goal} key={goal.id} />
-                ))}
-              </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 pb-4 pr-2">
+              {result.goals.map((goal) => (
+                <GoalCard key={goal.id} goal={goal} />
+              ))}
             </div>
           ) : (
-            <GoalsNotFound pageHasParams />
+            <GoalsNotFound pageHasParams={!!query} />
           )}
         </div>
       </div>
-      <RoutePaginationControls {...result} />
+      {result.totalPages > 1 && <RoutePaginationControls {...result} />}
     </main>
   );
 };
