966cc4b0421452bb786e3784d6750e8e0ae70677
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-02-21 22:33:57 +0300

    Refactor login and signup pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
 import LoginForm from "@/components/auth/login-form";
+import { getUser } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { user } = await getUser();
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center p-4 gap-4">
       <div className="w-full md:w-[700px]">
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,20 @@
-import SignUpClient from "./SignUpClient";
+import { getUser } from "@/lib/session";
+import { redirect } from "next/navigation";
+import SignUpForm from "@/components/auth/sign-up-form";
 
-interface ISignUpPage {}
+const SignUpPage = async () => {
+  const { user } = await getUser();
+  if (user) {
+    redirect("/dashboard");
+  }
 
-const SignUpPage = ({}: ISignUpPage) => {
-  return <SignUpClient />;
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <div className="w-full md:w-[700px]">
+        <SignUpForm />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
