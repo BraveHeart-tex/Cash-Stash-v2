4d80c6ea3b3880e700e674bc24a270eb1c1b12e1
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-02-21 23:42:36 +0300

    Remove unused Redux slices

--- redux/store.ts
+++ redux/store.ts
@@ -1,24 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userAccountReducer from "./features/userAccountSlice";
-import currentAccountReducer from "./features/currentAccountSlice";
-import userReducer from "./features/userSlice";
-import budgetReducer from "./features/budgetSlice";
-import currentBudgetReducer from "./features/currentBudgetSlice";
-import currentGoalReducer from "./features/currentGoalSlice";
-import goalReducer from "./features/goalSlice";
 import genericConfirmReducer from "./features/genericConfirmSlice";
 import genericModalReducer from "./features/genericModalSlice";
 import navigationTabsReducer from "./features/navigationTabsSlice";
 
 export const store = configureStore({
   reducer: {
-    userAccountReducer,
-    userReducer,
-    currentAccountReducer,
-    budgetReducer,
-    currentBudgetReducer,
-    goalReducer,
-    currentGoalReducer,
     genericConfirmReducer,
     genericModalReducer,
     navigationTabsReducer,
