7123f18844ce33ab8b74550a39662dc29fa4c649
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-02-29 15:36:55 +0300

    Implement timer functionality for entering 2-factor authentication code. Implement UI for entering the 2-factor authentication code. Update constants.ts to include TWO_FACTOR_AUTH_INPUT_TIMEOUT_SECONDS

--- components/auth/TwoFactorAuthenticationForm.tsx
+++ components/auth/TwoFactorAuthenticationForm.tsx
@@ -3,6 +3,18 @@
 import AutoProgressInput from "@/components/auto-progress-input";
 import { useEffect, useState, useTransition } from "react";
 import { validateOTP } from "@/data/auth";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import logo from "@/components/Logo.svg";
+import { Label } from "@/components/ui/label";
+import TwoFactorAuthenticationTimer from "@/components/two-factor-authentication-timer";
 
 const TwoFactorAuthenticationForm = ({ email }: { email: string }) => {
   const [isPending, startTransition] = useTransition();
@@ -17,15 +29,35 @@ const TwoFactorAuthenticationForm = ({ email }: { email: string }) => {
   }, [code]);
 
   return (
-    <div className="flex flex-col gap-2 w-full">
-      <h1>Two Factor Authentication</h1>
-      <p>Please enter the 6-digit code from your authenticator app.</p>
-      {isPending ? (
-        <p>Validating...</p>
-      ) : (
-        <AutoProgressInput length={6} onChange={setCode} />
-      )}
-    </div>
+    <Card>
+      <CardHeader className="text-xl">
+        <Image
+          src={logo}
+          alt="Cash Stash"
+          width={200}
+          className="mb-4 md:mx-auto dark:invert"
+        />
+        <CardTitle>Two-Factor Authentication</CardTitle>
+        <CardDescription>
+          Your account is protected with two-factor authentication. Please enter
+          the 6-digit code from your authenticator app to log in.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <TwoFactorAuthenticationTimer />
+        <div className="w-full flex items-center justify-center mt-4">
+          {isPending ? (
+            <p>Validating...</p>
+          ) : (
+            <div>
+              <Label>Your 6-digit code</Label>
+              <AutoProgressInput length={6} onChange={setCode} />
+            </div>
+          )}
+        </div>
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
   );
 };
 
