725effef05a857328e9045d6337965545e5fd8d9
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-03-08 16:23:01 +0300

    Remove unused forms for reminder entity.

--- lib/getGenericModalContent.ts
+++ lib/getGenericModalContent.ts
@@ -1,5 +1,3 @@
-import CreateReminderForm from "@/components/reminders/forms/create-reminder-form";
-import EditReminderForm from "@/components/reminders/forms/edit-reminder-form";
 import { createElement } from "react";
 import AccountForm from "@/components/accounts/account-form";
 import BudgetForm from "@/components/budgets/budget-form";
@@ -20,10 +18,6 @@ const ContentMap: {
   budget: BudgetForm,
   goal: GoalForm,
   transaction: TransactionForm,
-  reminder: {
-    create: CreateReminderForm,
-    edit: EditReminderForm,
-  },
   account: AccountForm,
 };
 
