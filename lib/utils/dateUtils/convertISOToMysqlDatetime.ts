bb931129f662e751484f4023c6f6ec6845d6b76c
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-03-17 18:40:36 +0300

    Add convertISOToMysqlDatetime function to handle ISO datetime conversion

---
+++ lib/utils/dateUtils/convertISOToMysqlDatetime.ts
@@ -0,0 +1,12 @@
+export function convertISOToMysqlDatetime(isoDatetimeString: string) {
+  const isoDatetime = new Date(isoDatetimeString);
+
+  const year = isoDatetime.getFullYear();
+  const month = ("0" + (isoDatetime.getMonth() + 1)).slice(-2); // Months are zero-based
+  const day = ("0" + isoDatetime.getDate()).slice(-2);
+  const hours = ("0" + isoDatetime.getHours()).slice(-2);
+  const minutes = ("0" + isoDatetime.getMinutes()).slice(-2);
+  const seconds = ("0" + isoDatetime.getSeconds()).slice(-2);
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
