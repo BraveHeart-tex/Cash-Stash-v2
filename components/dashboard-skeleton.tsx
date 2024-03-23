a52748556d8ae6a5b63ad7e779f7e31c9657cb31
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-03-23 16:53:27 +0300

    Update dashboard and notification settings UI

--- components/dashboard-skeleton.tsx
+++ components/dashboard-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "./ui/skeleton";
 const DashboardSkeleton = () => {
   return (
     <div className="p-0 lg:p-1 lg.pt-0 mx-auto lg:max-w-[1300px] xl:max-w-[1600px]">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
         {Array.from({ length: 6 }).map((_, index) => (
           <Skeleton key={index} className="h-[30rem] w-[30rem]" />
         ))}
--- components/settings/notification-settings.tsx
+++ components/settings/notification-settings.tsx
@@ -1,8 +1,14 @@
+import { Label } from "../ui/label";
+import { Switch } from "../ui/switch";
+
 const NotificationSettings = () => {
   return (
     <div>
       <div>
-        <h2 className="text-xl font-semibold text-primary">Notifications</h2>
+        <h2 className="text-xl font-semibold text-primary flex items-center gap-2">
+          Notifications:
+          <Switch />
+        </h2>
         <p className="text-muted-foreground">
           Manage your notification preferences. We'll send you notifications
           only when you set a reminder for them.
