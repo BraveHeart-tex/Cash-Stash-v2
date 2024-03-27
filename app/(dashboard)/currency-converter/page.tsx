adc4348a5181a4e093a400778340e9079d7052e7
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-03-27 22:07:23 +0300

    Add user authentication to CurrencyConverterPage

--- app/(dashboard)/currency-converter/page.tsx
+++ app/(dashboard)/currency-converter/page.tsx
@@ -1,5 +1,25 @@
-const CurrencyConverterPage = () => {
-  return <div>CurrencyConverterPage</div>;
+import { getUser } from "@/lib/auth/session";
+import { PAGE_ROUTES } from "@/lib/constants";
+import { redirect } from "next/navigation";
+
+const CurrencyConverterPage = async () => {
+  const { user } = await getUser();
+
+  if (!user) {
+    redirect(PAGE_ROUTES.LOGIN_ROUTE);
+  }
+
+  return (
+    <main className="p-4 mx-auto lg:max-w-[1300px] xl:max-w-[1600px]">
+      <div className="flex flex-col gap-1">
+        <h2 className="text-4xl text-primary">Currency Converter</h2>
+        <p className="text-muted-foreground">
+          See the current exchange rates and convert between your preferred
+          currencies.
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default CurrencyConverterPage;
