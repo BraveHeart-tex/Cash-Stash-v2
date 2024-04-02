cfbe0413ca80288b46320af50e9a9bf44fa16dba
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-02 17:55:31 +0300

    Add loading components for authentication pages

---
+++ app/auth/help/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center p-4 gap-4">
+      <div className="w-full md:w-[500px] space-y-2 shadow-sm p-2 rounded-md">
+        <Skeleton className="mx-auto w-[200px] h-[75px] rounded-md" />
+        <div className="flex items-center justify-between gap-2">
+          <Skeleton className="w-full h-[50px] rounded-md" />
+          <Skeleton className="w-full h-[50px] rounded-md" />
+        </div>
+        <div className="flex items-center justify-between">
+          <Skeleton className="w-[100px] h-[35px] rounded-md" />
+          <Skeleton className="w-[100px] h-[35px] rounded-md" />
+        </div>
+        <Skeleton className="w-[100px] h-[35px] rounded-md ml-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
