7265beea6d4705287744e143cf78b3f423a726e9
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-11 09:41:37 +0300

    Add CurrencyConverterPageSkeleton component

--- app/(dashboard)/currency-converter/loading.tsx
+++ app/(dashboard)/currency-converter/loading.tsx
@@ -1,5 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const CurrencyConverterPageSkeleton = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="p-4 mx-auto lg:max-w-[1300px] xl:max-w-[1600px]">
+      <Skeleton className="w-[150px] h-[50px]" />
+      <Skeleton className="w-full lg:w-[400px] h-[50px] rounded-md mt-4" />
+      <Skeleton className="w-full lg:w-[500px] h-[50px] rounded-md mt-2" />
+      <Skeleton className="w-full h-[300px] lg:h-[500px] rounded-md mt-2" />
+    </div>
+  );
 };
 
 export default CurrencyConverterPageSkeleton;
