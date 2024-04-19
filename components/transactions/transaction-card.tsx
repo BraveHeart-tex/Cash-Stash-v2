9e2f0bf4fcf46e65bf196404b1c06e5906778076
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-19 18:55:39 +0300

    Refactor motionConfig in TransactionCard

--- components/transactions/transaction-card.tsx
+++ components/transactions/transaction-card.tsx
@@ -88,10 +88,7 @@ const TransactionCard = ({
   };
 
   const motionConfig: HTMLMotionProps<"article"> = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-    transition: { duration: 0.5 },
-    exit: { opacity: 0, y: 20 },
+    transition: { duration: 0.8 },
   };
 
   if (useLayoutId) {
