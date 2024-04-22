a6ce6dfabdd836ae92fff5628cb3064cd9ac5d6a
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-22 14:48:14 +0300

    Remove unused motion import in navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,6 @@
 import UserMenu from "@/components/user-menu";
 import Link from "next/link";
 import { useEffect } from "react";
-import { motion } from "framer-motion";
 import { User } from "lucia";
 import useAuthStore from "@/store/auth/authStore";
 import { PAGE_ROUTES } from "@/lib/constants";
@@ -19,28 +18,22 @@ const Navbar = ({ user }: { user: User | null }) => {
 
   return (
     <div className="mb-6 bg-primary p-4 shadow-sm dark:border-b dark:bg-background">
-      <motion.div
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.3, type: "just" }}
-      >
-        <div className="flex items-center justify-between">
-          <div className="hidden md:block" />
-          <Link href={PAGE_ROUTES.HOME_PAGE} className="ml-auto lg:ml-0">
-            <Logo
-              width={200}
-              height={200}
-              className="mx-auto grayscale invert monokai-dark:invert-0 lg:mx-0"
-            />
-          </Link>
-          <div className="ml-auto flex items-center justify-center gap-4 lg:ml-0">
-            <div className="hidden items-center gap-1 rounded-md bg-background lg:flex">
-              <ModeToggle />
-            </div>
-            <UserMenu />
+      <div className="flex items-center justify-between">
+        <div className="hidden md:block" />
+        <Link href={PAGE_ROUTES.HOME_PAGE} className="ml-auto lg:ml-0">
+          <Logo
+            width={200}
+            height={200}
+            className="mx-auto grayscale invert monokai-dark:invert-0 lg:mx-0"
+          />
+        </Link>
+        <div className="ml-auto flex items-center justify-center gap-4 lg:ml-0">
+          <div className="hidden items-center gap-1 rounded-md bg-background lg:flex">
+            <ModeToggle />
           </div>
+          <UserMenu />
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
