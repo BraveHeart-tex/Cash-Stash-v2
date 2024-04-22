14ead68a203b5f4f9bfa9e5ff45099e4f8353e18
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-22 19:39:00 +0300

    Update budget cards list height and overflow properties

--- components/budgets/budget-cards-list.tsx
+++ components/budgets/budget-cards-list.tsx
@@ -7,7 +7,7 @@ type BudgetCardsListProps = {
 
 const BudgetCardsList = ({ budgets }: BudgetCardsListProps) => {
   return (
-    <div className="h-[500px] overflow-auto pt-2">
+    <div className="mt-2 h-[calc(100vh-450px)] overflow-auto lg:h-[calc(100vh-400px)]">
       <div className="grid grid-cols-1 gap-4 pb-4 pr-2 lg:grid-cols-2 xl:grid-cols-3">
         {budgets.map((budget) => (
           <BudgetCard key={budget.id} budget={budget} />
