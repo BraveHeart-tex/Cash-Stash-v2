e97425f973050a47137f13eb035181f3116c872f
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-22 20:50:56 +0300

    Refactor GoalCardsList component layout

--- components/goals/goal-cards-list.tsx
+++ components/goals/goal-cards-list.tsx
@@ -7,15 +7,12 @@ type GoalCardsListProps = {
 
 const GoalCardsList = ({ goals }: GoalCardsListProps) => {
   return (
-    <div
-      className="mt-2 grid h-[calc(100vh-450px)] grid-cols-1 gap-4 overflow-auto overflow-y-auto pb-4 pr-2 pt-2 lg:h-[calc(100vh-400px)] lg:grid-cols-2 xl:grid-cols-3"
-      style={{
-        gridAutoRows: "min-content",
-      }}
-    >
-      {goals.map((goal) => (
-        <GoalCard key={goal.id} goal={goal} />
-      ))}
+    <div className="mt-2 h-[calc(100vh-400px)] overflow-y-auto pb-4 pr-2 pt-2 lg:h-[calc(100vh-400px)]">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+        {goals.map((goal) => (
+          <GoalCard key={goal.id} goal={goal} />
+        ))}
+      </div>
     </div>
   );
 };
