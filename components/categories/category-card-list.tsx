77ff75482751ffed1fa8d82c6cc1eed354d74ce7
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-22 20:57:30 +0300

    Refactor category card list layout

--- components/categories/category-card-list.tsx
+++ components/categories/category-card-list.tsx
@@ -6,10 +6,12 @@ type CategoryCardListProps = {
 };
 const CategoryCardList = ({ categories }: CategoryCardListProps) => {
   return (
-    <div className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
-      {categories.map((category) => (
-        <CategoryCard key={category.id} category={category} />
-      ))}
+    <div className="mt-2 h-[calc(100vh-510px)] w-full overflow-auto pr-2 lg:h-[calc(100vh-450px)]">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+        {categories.map((category) => (
+          <CategoryCard key={category.id} category={category} />
+        ))}
+      </div>
     </div>
   );
 };
