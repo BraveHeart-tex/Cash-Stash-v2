16e96fc661d79a14276aec459e5b336ec5f296bb
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-29 22:19:02 +0300

    Refactor preferred currency settings to use translated currencies

--- components/settings/preferred-currency-settings.tsx
+++ components/settings/preferred-currency-settings.tsx
@@ -1,5 +1,5 @@
 import CurrencyCombobox from "@/components/settings/currency-combobox";
-import { CURRENCIES } from "@/lib/constants";
+import { getTranslations } from "next-intl/server";
 
 type PreferredCurrencySettingsProps = {
   title: string;
@@ -10,18 +10,24 @@ const PreferredCurrencySettings = async ({
   title,
   description,
 }: PreferredCurrencySettingsProps) => {
+  const t = await getTranslations("Lists");
+
+  const mappedCurrencies = t("currencies")
+    .split(", ")
+    .map((s) => s.split(":"))
+    .map(([name, symbol]) => ({ name, symbol }))
+    .map(({ name, symbol }) => ({
+      label: name + " (" + symbol + ")",
+      value: symbol,
+    }));
+
   return (
     <section id="preferred-currency" className="flex flex-col gap-2">
       <div>
         <h2 className="text-xl font-semibold text-primary">{title}</h2>
         <p className="text-muted-foreground">{description}</p>
       </div>
-      <CurrencyCombobox
-        currencies={CURRENCIES.map((item) => ({
-          label: item.name + " (" + item.symbol + ")",
-          value: item.symbol,
-        }))}
-      />
+      <CurrencyCombobox currencies={mappedCurrencies} />
     </section>
   );
 };
