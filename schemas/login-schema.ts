eca3cd26acc351ee930f98a7e370945051d4fed3
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-04-29 11:48:05 +0300

    Refactor login schema to include message configuration

--- schemas/login-schema.ts
+++ schemas/login-schema.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
+type LoginSchemaMessageConfig = {
+  invalidEmail: string;
+  passwordTooShort: string;
+  passwordTooLong: string;
+};
+
 export const getLoginSchema = (messageConfig: LoginSchemaMessageConfig) => {
   const { invalidEmail, passwordTooShort, passwordTooLong } = messageConfig;
   return z.object({
     email: z.string().email(invalidEmail),
     password: z.string().min(8, passwordTooShort).max(100, passwordTooLong),
   });
-};
-
-type LoginSchemaMessageConfig = {
-  invalidEmail: string;
-  passwordTooShort: string;
-  passwordTooLong: string;
 };
 
 export type LoginSchemaType = z.infer<ReturnType<typeof getLoginSchema>>;
