b839fac7314425bef79004b2369b3c89483a7f32
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-05-04 10:57:30 +0300

    Refactor budget page filters and add translation support

--- components/budgets/budgets-page-filters.tsx
+++ components/budgets/budgets-page-filters.tsx
@@ -11,62 +11,63 @@ type BudgetsPageFiltersProps = {
   initialBudgetCategories: CategorySelectModel[];
 };
 
-const routeFilterOptions = [
-  {
-    label: "Sort by Remaining (Low to High)",
-    icon: <FaPiggyBank className="mr-2" />,
-    data: {
-      sortBy: "progress",
-      sortDirection: "desc",
-    },
-  },
-  {
-    label: "Sort by Remaining (High to Low)",
-    icon: <FaPiggyBank className="mr-2" />,
-    data: {
-      sortBy: "progress",
-      sortDirection: "asc",
-    },
-  },
-  {
-    label: "Sort by Spending (High to Low)",
-    icon: <GiPayMoney className="mr-auto" />,
-    data: {
-      sortBy: "spentAmount",
-      sortDirection: "desc",
-    },
-  },
-  {
-    label: "Sort by Spending (Low to High)",
-    icon: <GiPayMoney className="mr-auto" />,
-    data: {
-      sortBy: "spentAmount",
-      sortDirection: "asc",
-    },
-  },
-  {
-    label: "Sort by Budget (High to Low)",
-    icon: <FaPiggyBank className="mr-2" />,
-    data: {
-      sortBy: "budgetAmount",
-      sortDirection: "desc",
-    },
-  },
-  {
-    label: "Sort by Budget (Low to High)",
-    icon: <FaPiggyBank className="mr-2" />,
-    data: {
-      sortBy: "budgetAmount",
-      sortDirection: "asc",
-    },
-  },
-];
-
 const BudgetsPageFilters = ({
   budgets,
   initialBudgetCategories,
 }: BudgetsPageFiltersProps) => {
   const t = useTranslations("Budgets.BudgetsPageFilters");
+
+  const routeFilterOptions = [
+    {
+      label: t("sortByProgressDesc"),
+      icon: <FaPiggyBank className="mr-2" />,
+      data: {
+        sortBy: "progress",
+        sortDirection: "desc",
+      },
+    },
+    {
+      label: t("sortByProgressAsc"),
+      icon: <FaPiggyBank className="mr-2" />,
+      data: {
+        sortBy: "progress",
+        sortDirection: "asc",
+      },
+    },
+    {
+      label: t("sortBySpentAmountDesc"),
+      icon: <GiPayMoney className="mr-auto" />,
+      data: {
+        sortBy: "spentAmount",
+        sortDirection: "desc",
+      },
+    },
+    {
+      label: t("sortBySpentAmountAsc"),
+      icon: <GiPayMoney className="mr-auto" />,
+      data: {
+        sortBy: "spentAmount",
+        sortDirection: "asc",
+      },
+    },
+    {
+      label: t("sortByBudgetAmountDesc"),
+      icon: <FaPiggyBank className="mr-2" />,
+      data: {
+        sortBy: "budgetAmount",
+        sortDirection: "desc",
+      },
+    },
+    {
+      label: t("sortByBudgetAmountAsc"),
+      icon: <FaPiggyBank className="mr-2" />,
+      data: {
+        sortBy: "budgetAmount",
+        sortDirection: "asc",
+      },
+    },
+  ];
+
   return (
     <div className="flex flex-wrap items-center justify-between gap-2">
       <div className="flex items-center gap-2">
