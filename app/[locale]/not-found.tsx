92721e722d7aa09c8c18c44d5a49e099c460545d
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-05-16 16:26:32 +0300

    Refactor 404 not found page logic

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,26 +1,19 @@
 import { Button } from "@/components/ui/button";
-import { getUser } from "@/lib/auth/session";
 import { PAGE_ROUTES } from "@/lib/constants";
-import { redirect } from "@/navigation";
 import { Link } from "@/navigation";
+import { useTranslations } from "next-intl";
 
-const NotFound = async () => {
-  const { user } = await getUser();
-  if (!user) {
-    return redirect(PAGE_ROUTES.LOGIN_ROUTE);
-  }
-
+const NotFound = () => {
+  const t = useTranslations("NotFound");
   return (
-    <div className="mx-auto mb-2 p-1 lg:max-w-[1300px] lg:p-4 xl:max-w-[1600px]">
-      <div className="grid grid-cols-1 gap-4">
+    <div className="mx-auto mb-2 flex h-screen flex-col items-center justify-center p-1 lg:max-w-[1300px] lg:p-4 xl:max-w-[1600px]">
+      <div className="grid grid-cols-1 gap-4 p-2 text-center">
         <h1 className="text-left text-6xl font-semibold text-primary">
-          404 Page Not Found...
+          {t("title")}
         </h1>
-        <p className="text-foreground">
-          Looks like you tried to navigate to a page that doesn&apos;t exist.
-        </p>
-        <Button className="w-max">
-          <Link href={PAGE_ROUTES.HOME_PAGE}>Back to the home page</Link>
+        <p className="text-muted-foreground">{t("description")}</p>
+        <Button type="button" name="go-home">
+          <Link href={PAGE_ROUTES.HOME_PAGE}>{t("linkLabel")}</Link>
         </Button>
       </div>
     </div>
